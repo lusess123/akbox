86f70bbc55d0e3bc016efae90decfa6ac80b7d9b
郑瑜琨 <zhengyukun@lvwan.com>
2018-09-24 01:53:00 +0800

    link

--- packages/hosts/umihost/pages/index.tsx
+++ packages/hosts/umihost/pages/index.tsx
@@ -2,8 +2,12 @@ import  React from "react";
 import TsPage from "akbox/tspage";
 
 export default ()=>{
-  return <>
+  return  <>
             <div>ddddd123iiiiii</div>
             <TsPage  name="sdsd" ></TsPage>
+           <p> <a href="/akbox/vm">简单渲染 demo</a></p>
+           <p>
+            <a href="/akbox/todolist">todolist demo</a>
+            </p>
          </>
 }
