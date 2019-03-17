fe853fae19328a3079eb833ba83e591f97f31ae3
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-03-17 13:42:21 +0800

    code page 链接

--- packages/hosts/umihost/pages/index.tsx
+++ packages/hosts/umihost/pages/index.tsx
@@ -9,6 +9,8 @@ export default ()=>{
            <p> <a href="/akbox/vm">简单渲染 demo</a></p>
            <p>
             <a href="/akbox/todolist">todolist demo</a>
+            <br/>
+            <a href="/ak/codepage">代码生成器</a>
             </p>
          </>
 }
